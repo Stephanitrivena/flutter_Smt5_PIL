4b85bee76d4e140ab0007d143e6d01a18fe41cf1
Stephanitrivena <stephanitrivena12@gmail.com>
2022-11-09 19:23:49 +0700

    task management

--- lib/app/modules/friends/views/friends_view.dart
+++ lib/app/modules/friends/views/friends_view.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
-import '../controllers/about_controller.dart';
+import '../controllers/friends_controller.dart';
 
-class AboutView extends GetView<AboutController> {
-  const AboutView({Key? key}) : super(key: key);
+class FriendsView extends GetView<FriendsController> {
+  const FriendsView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AboutView'),
+        title: const Text('FriendsView'),
         centerTitle: true,
       ),
       body: Center(
         child: Text(
-          'AboutView is working',
+          'FriendsView is working',
           style: TextStyle(fontSize: 20),
         ),
       ),
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -6,6 +6,9 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const PROFILE = _Paths.PROFILE;
   static const ABOUT = _Paths.ABOUT;
+  static const LOGIN = _Paths.LOGIN;
+  static const TASK = _Paths.TASK;
+  static const FRIENDS = _Paths.FRIENDS;
 }
 
 abstract class _Paths {
@@ -13,4 +16,7 @@ abstract class _Paths {
   static const HOME = '/home';
   static const PROFILE = '/profile';
   static const ABOUT = '/about';
+  static const LOGIN = '/login';
+  static const TASK = '/task';
+  static const FRIENDS = '/friends';
 }
